a9eb7b75329b23aae9c2528b1f1f4e8defde5a3d
Alex Lexa <alex.lexalexy@gmail.com>
2024-12-14 19:01:33 +0100

    Optimisation du code

--- Denombrements/Program.cs
+++ Denombrements/Program.cs
@@ -1,89 +1,106 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Denombrements
 {
     class Program
     {
+        /// <summary>
+        /// Programme permettant de calculer des permutations, arrangements et combinaisons.
+        /// </summary>
         static void Main(string[] args)
         {
-            int c = 1;
-            while (c != 0)
+            // Initialisation de la variable de choix
+            int choix = 1;
+            // Boucle principale du programme, jusqu'à ce que l'utilisateur choisisse de quitter
+            while (choix != 0)
             {
-
                 try
                 {
+                    // Affichage du menu des options disponibles
                     Console.WriteLine("Permutation ...................... 1");
                     Console.WriteLine("Arrangement ...................... 2");
                     Console.WriteLine("Combinaison ...................... 3");
                     Console.WriteLine("Quitter .......................... 0");
                     Console.Write("Choix :                            ");
-                    c = int.Parse(Console.ReadLine());
-                    if (c == 0)
+                    choix = int.Parse(Console.ReadLine());
+                    // Si l'utilisateur choisit de quitter, on termine le programme
+                    if (choix == 0)
                     {
                         Environment.Exit(0);
                     }
-                    if (c == 1)
+                    // Calcul de la permutation (si l'utilisateur choisit 1)
+                    if (choix == 1)
                     {
-                        // le nombre d'éléments à gérer
-                        Console.Write("nombre total d'éléments à gérer = ");
-                        // saisir le nombre
-                        int n = int.Parse(Console.ReadLine());
+                        int nombreTotalElements = GetTotalElements();
                         // calcul de r                                       
                         long r = 1;
-                        for (int k = 1; k <= n; k++)
+                        for (int k = 1; k <= nombreTotalElements; k++)
                             r *= k;
-                        Console.WriteLine(n + "! = " + r);
+                        Console.WriteLine(nombreTotalElements + "! = " + r);
                     }
-                    if (c == 2)
+                    // Calcul de l'arrangement (si l'utilisateur choisit 2)
+                    if (choix == 2)
                     {
-                        // le nombre d'éléments à gérer
-                        Console.Write("nombre total d'éléments à gérer = ");
-                        // saisir le nombre
-                        int t = int.Parse(Console.ReadLine());
-                        // le sous ensemble
-                        Console.Write("nombre d'éléments dans le sous ensemble = ");
-                        // saisir le nombre
-                        int n = int.Parse(Console.ReadLine());
-
+                        int nombreTotalElements = GetTotalElements();
+                        int nombreSubsetElements = GetSubsetSize();
                         // calcul de r
                         long r = 1;
-                        for (int k = (t - n + 1); k <= t; k++)
+                        for (int k = (nombreTotalElements - nombreSubsetElements + 1); k <= nombreTotalElements; k++)
                             r *= k;
-                        Console.WriteLine("A(" + t + "/" + n + ") = " + r);
+                        Console.WriteLine("A(" + nombreTotalElements + "/" + nombreSubsetElements + ") = " + r);
                     }
-                    if (c == 3)
+                    // Calcul de la combinaison (si l'utilisateur choisit 3)
+                    if (choix == 3)
                     {
-                        // le nombre d'éléments à gérer
-                        Console.Write("nombre total d'éléments à gérer = ");
-                        // saisir le nombre
-                        int t = int.Parse(Console.ReadLine());
-                        // le sous ensemble
-                        Console.Write("nombre d'éléments dans le sous ensemble = ");
-                        // saisir le nombre
-                        int n = int.Parse(Console.ReadLine());
+                        int nombreTotalElements = GetTotalElements();
+                        int nombreSubsetElements = GetSubsetSize();
                         // calcul de r1
                         long r1 = 1;
-                        for (int k = (t - n + 1); k <= t; k++)
+                        for (int k = (nombreTotalElements - nombreSubsetElements + 1); k <= nombreTotalElements; k++)
                             r1 *= k;
                         // calcul de r2
                         long r2 = 1;
-                        for (int k = 1; k <= n; k++)
+                        for (int k = 1; k <= nombreSubsetElements; k++)
                             r2 *= k;
                         // calcul de r3
-                        //Console.WriteLine("résultat = " + (r1 / r2));
-                        Console.WriteLine("C(" + t + "/" + n + ") = " + (r1 / r2));
+                        Console.WriteLine("C(" + nombreTotalElements + "/" + nombreSubsetElements + ") = " + (r1 / r2));
+                    }
+                    // Si l'utilisateur entre une valeur incorrecte
+                    else if (choix > 3 || choix < 0 )
+                    {
+                        Console.WriteLine("Merci de répondre par 1;2;3 ou 0");
                     }
                 }
                 catch
                 {
+                    // Si une erreur se produit lors de la saisie
                     Console.WriteLine("Merci de répondre par 1;2;3 ou 0");
                 }
             }
+            // Attente de la saisie de l'utilisateur avant de quitter
             Console.ReadLine();
+        }
+        /// <summary>
+        /// Fonction qui demande à l'utilisateur combien d'éléments il y a à gérer
+        /// </summary>
+        /// <returns>Le nombre total d'éléments à gérer.</returns>
+        static int GetTotalElements()
+        {
+            // Demande du nombre total d'éléments à gérer
+            Console.Write("nombre total d'éléments à gérer = ");
+            int nombreTotalElements = int.Parse(Console.ReadLine());
+            return nombreTotalElements;
+        }
+        /// <summary>
+        /// Fonction qui demande à l'utilisateur combien d'éléments il y a dans le sous-ensemble
+        /// </summary>
+        /// <returns>Le nombre d'éléments dans le sous-ensemble.</returns>
+        static int GetSubsetSize()
+        {
+            // Demande du nombre d'éléments dans le sous-ensemble
+            Console.Write("nombre d'éléments dans le sous ensemble = ");
+            int nombreSubsetElements = int.Parse(Console.ReadLine());
+            return nombreSubsetElements;
         }
     }
 }
